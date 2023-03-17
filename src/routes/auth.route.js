201a972fd27f1ef1a95556f8c4cfb308c68a018a
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-03-18 00:13:00 +0100

    addle table logic

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,4 +2,7 @@ import express from 'express';
 
 const authRouter = express.Router();
 
+// POST /register: Creates a new user account
+// POST /login: Logs a user into their account
+
 export default authRouter;
---
+++ src/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const userRouter = express.Router();
+
+// GET /users/:id: Retrieves a specific user by their ID
+// PUT /users/:id: Updates a user's account information by their ID
+
+export default userRouter;
