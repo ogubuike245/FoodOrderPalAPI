afc3220c83bcba6b0399d26db345fe2e90f92f5f
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-03-18 00:03:39 +0100

    added middleware for erro handling and request methods allowed

---
+++ src/config/config.js
@@ -0,0 +1,30 @@
+// Importing the Mongoose package
+import mongoose from 'mongoose';
+
+// Extracting the API_PORT and MONG0_DB_URI environment variables
+const { API_PORT, MONG0_DB_URI } = process.env;
+
+// Defining an asynchronous function that connects to a MongoDB database
+const connectToDatabase = async (app) => {
+    try {
+        // Connecting to the MongoDB database using the MONG0_DB_URI and some options
+        await mongoose.set('strictQuery', false).connect(MONG0_DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        // Starting the app and listening on the API_PORT environment variable (or port 9000 by default)
+        app.listen(API_PORT || 9000, () => {
+            // Logging a message indicating that the app is running and connected to the database
+            console.log(
+                `BACKEND SERVER RUNNING ON PORT : ${API_PORT} AND CONNECTED TO MONGODB DATABASE`
+            );
+        });
+    } catch (error) {
+        // Logging any errors that occur during the database connection or app start-up
+        console.log(error);
+    }
+};
+
+// Exporting the connectToDatabase function so it can be used in other parts of the application
+export default connectToDatabase;
